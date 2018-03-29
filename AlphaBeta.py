da6a36acab6638434faa744b7de2ebfa640300cc
Oliver Zhang <34319335+oliverzhang42@users.noreply.github.com>
2018-03-29 12:39:16 -0700

    Add files via upload

---
+++ AlphaBeta.py
@@ -0,0 +1,160 @@
+#This is Alpha Beta Pruning for Othello, using
+#https://github.com/hylbyj/Alpha-Beta-Pruning-for-Othello-Game/blob/master/othello.py
+#ply is the depth
+
+from ReversiAI import ReversiController, process
+import numpy as np
+from othelloBoard import Board
+
+path = "/Users/student36/Desktop/ReinforcementLearning/Reversi1/"
+#path = "/home/oliver/Desktop/Reversi3/"
+
+controller = ReversiController(path, False, False, 1)
+controller.load([40000])
+
+def reverse(board):
+    newBoard = [[0 for i in range(8)] for j in range(8)]
+    d = {0:0, 1:-1, -1:1}
+    for i in range(8):
+        for j in range(8):
+            newBoard[i][j] = d[board[i][j]]
+
+    return newBoard
+
+
+class AlphaBeta():
+    def __init__(self):
+        pass
+    
+    def policy(self, board, color):
+        global controller
+        if(color == -1):
+            board = reverse(board)
+        processedBoard = process(board)
+        processedBoard = np.array([processedBoard])
+        return controller.population[0].model.predict(processedBoard)[0][0]
+
+    #How I implement minimax algorithm:
+    #I separated minimax into three functions, the first one is minimax which is the general function acting like get the min or the max value based on the depth it currently explores:
+    def _minmax(self, board, color, ply):
+        #need to get all the legal moves
+        moves = board.get_legal_moves(color)
+
+        print("legal move " + str(moves))
+        if not isinstance(moves, list):
+           score = self.policy(board, color)
+           return score,None
+        print(ply)
+        return_move = moves[0]
+        bestscore = - StudentEngine.INFINITY
+        print("using minmax best score: "+ str(bestscore))
+        #ply = 4
+        #will define ply later;
+        for move in moves:
+            newboard = deepcopy(board)
+            newboard.execute_move(move,color)
+
+            score = self.min_score(newboard, -color, ply)
+            if score > bestscore:
+                bestscore = score
+                return_move = move
+                print("return move " + str(return_move) + "bestscore " + str(bestscore))
+        #newboard = deepcopy(board)
+        #return max((value(newboard.execute_move(m, color)),m) for m in moves)
+        return (bestscore,return_move)
+
+    #function max_score is aimed to maximize the score of player and function min_score is aimed to minimize the score of opponent. They all only return the value.
+    def max_score(self, board, color, ply):
+        moves = board.get_legal_moves(color)
+        #if not isinstance(moves, list):
+        #   return board.count(color)
+        if ply == 0:
+           return self.policy(board, color)
+        bestscore = -StudentEngine.INFINITY
+        for move in moves:
+            newboard = deepcopy(board)
+            newboard.execute_move(move,color)
+            score = self.min_score(newboard, -color, ply-1)
+            if score > bestscore:
+                bestscore = score
+        return bestscore
+
+    def min_score(self, board, color, ply):
+        moves = board.get_legal_moves(color)
+        #if not isinstance(moves, list):
+        #   return board.count(color)
+        if ply == 0:
+           return self.policy(board, color)
+        bestscore = StudentEngine.INFINITY
+        for move in moves:
+            newboard = deepcopy(board)
+            newboard.execute_move(move,color)
+            score = self.max_score(newboard, -color, ply-1)
+            if score < bestscore:
+                bestscore = score
+        return bestscore
+
+    #2) Alpha-beta Othello player
+    #I modify the three functions and initially set alpha, beta as +infinity and -infinity. The functions are listed:
+    def _minmax_with_alpha_beta(self, board, color, ply):
+        moves = board.get_legal_moves(color)
+        #print "leagal move" + str(moves)
+        if not isinstance(moves, list):
+           score = board.count(color)
+           return score, None
+
+        #print ply
+        return_move = moves[0]
+        bestscore = - StudentEngine.INFINITY
+        #print "using alpha_beta best score:"+ str(bestscore)
+        #ply = 4
+        #will define ply later;
+        for move in moves:
+            newboard = deepcopy(board)
+            newboard.execute_move(move,color)
+
+            score = self.min_score_alpha_beta(newboard, -color, ply, StudentEngine.INFINITY, -StudentEngine.INFINITY)
+            if score > bestscore:
+               bestscore = score
+               return_move = move
+               #print "return move" + str(return_move) + "best score" + str(bestscore)
+
+        return (bestscore,return_move)
+
+    #Also the max and min value function:
+    def max_score_alpha_beta(self, board, color, ply, alpha, beta):
+        if ply == 0:
+            return self.policy(board, color)
+        bestscore = -StudentEngine.INFINITY
+        for move in board.get_legal_moves(color):
+            newboard = deepcopy(board)
+            newboard.execute_move(move,color)
+            score = self.min_score_alpha_beta(newboard, -color, ply-1, alpha, beta)
+            if score > bestscore:
+                bestscore = score
+            if bestscore >= beta:
+                return bestscore
+            alpha = max (alpha,bestscore)
+        return bestscore
+
+    def min_score_alpha_beta(self, board, color, ply, alpha, beta):
+          if ply == 0:
+             return self.policy(board, color)
+          bestscore = StudentEngine.INFINITY
+          for move in board.get_legal_moves(color):
+              newboard = deepcopy(board)
+              newboard.execute_move(move,color)
+              score = self.max_score_alpha_beta(newboard, -color, ply-1, alpha, beta)
+              if score < bestscore:
+                 bestscore = score
+              if bestscore <= alpha:
+                 return bestscore
+              beta = min(beta,bestscore)
+          return bestscore
+
+
+x = AlphaBeta()
+board = Board()
+x.policy(array, 1)
+
+print(x._minmax_with_alpha_beta(board, 1, 5))
